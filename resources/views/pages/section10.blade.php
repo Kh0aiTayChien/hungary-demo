b0217dd0821d5e18d004528e9c8494c02ee67384
Chien.le <chien.hcckt@gmail.com>
2023-05-19 16:38:06 +0700

    fix-font and small error

--- resources/views/pages/section10.blade.php
+++ resources/views/pages/section10.blade.php
@@ -1,7 +1,7 @@
 <style>
     .section10 {
         width: 100vw;
-        height: 100vh;
+        height: 110vh;
         background-image: url('{{asset('images/sec10/sec10.png')}}');
         background-repeat: no-repeat;
         background-position: center;
@@ -85,38 +85,38 @@
         font-family: 'Mulish-ExtraBold', sans-serif;
         font-weight: bold;
         font-size: large;
-        letter-spacing: 0.5rem;
+        letter-spacing: 0.2rem;
     }
 
 
 </style>
-<div class="section10 d-flex justify-content-center " style="">
+<div class="section10 d-flex justify-content-center align-items-center " style="">
     <div class=" row " style="width: 60%; margin-top: 5%">
         <div class="col-6">
             <div>
                 <img src="{{asset('images/sec10/donvidoitac1.svg')}}" alt="" class="" style="width: 35%">
             </div>
             <div>
-                <img src="{{asset('images/sec10/cen-logo.svg')}}" alt="" class="cen-logo" style="width: 50%">
+                <img src="{{asset('images/sec10/cen-logo.svg')}}" alt="" class="cen-logo" style="width: 60%">
                 <img src="{{asset('images/sec10/danube.svg')}}" alt="" class="cen-logo"
-                     style="width: 50%; margin-top: -5%">
+                     style="width: 60%; margin-top: -5%">
             </div>
             <div>
-                <img src="{{asset('images/sec10/lienhevoichungtoi1.svg')}}" alt="" class="mt-5" style="width: 45%">
+                <img src="{{asset('images/sec10/lienhevoichungtoi1.svg')}}" alt="" class="mt-3" style="width: 45%">
             </div>
             <div class="mt-5">
                 <img src="{{asset('images/sec10/phone.svg')}}" alt="" class="" style="width: 5%">
-                <span class="text-white ms-3"> 096 569 1688</span>
+                <span class="text-white ms-3 mulish-semi"> 096 569 1688</span>
             </div>
             <div class="mt-2">
                 <img src="{{asset('images/sec10/mail.svg')}}" alt="" class="" style="width: 5%">
-                <span class="text-white ms-3"> huyynguyentherealadvisor@gmail.com</span>
+                <span class="text-white ms-3 mulish-semi"> huyynguyentherealadvisor@gmail.com</span>
             </div>
             <div class="mt-2">
                 <img src="{{asset('images/sec10/map.svg')}}" alt="" class="" style="width: 5%">
-                <span class="text-white ms-3"> Tầng 1 - 137  Nguyễn Ngọc Vũ</span>
+                <span class="text-white ms-3 mulish-semi"> Tầng 1 - 137  Nguyễn Ngọc Vũ</span>
                 <br>
-                <span class="text-white ms-5 "> Trung Hòa, Cầu Giấy, Hà Nội</span>
+                <span class="text-white ms-5 mulish-semi"> Trung Hòa, Cầu Giấy, Hà Nội</span>
             </div>
 
         </div>
@@ -131,7 +131,7 @@
             <form action="{{ route('send') }}"  method="POST" enctype="multipart/form-data"
                   class="form-inline mt-3" id="">
                 @csrf
-                <div class="form-group mt-1">
+                <div class="form-group mt-1 mulish-semi">
 
                     <input type="text" class="form-control " name="name" id="name"
                            placeholder="Họ và tên (bắt buộc)" required
@@ -181,12 +181,13 @@
 
         <form action="{{ route('send') }}"  method="POST" enctype="multipart/form-data" class="form-inline mt-3">
             @csrf
-            <div class="form-group mt-1">
+            <div class="form-group mt-1 mulish-semi" >
                 <label for="inputName" class="sr-only">Họ và tên:</label>
                 <input type="text" class="form-control " name="name" id="name"
                        placeholder="Họ và tên (bắt buộc)" required
                        oninvalid="this.setCustomValidity('Vui lòng nhập đầy đủ họ tên')"
-                       oninput="this.setCustomValidity('')" >
+                       oninput="this.setCustomValidity('')"
+                       style="font-size: 14px">
 
             </div>
             <div class="form-group  mt-2">
@@ -194,19 +195,22 @@
                 <input type="number" class="form-control " name="phone"
                        placeholder="Số điện thoại (bắt buộc)"
                        oninvalid="this.setCustomValidity('Vui lòng nhập số điện thoại')"
-                       oninput="this.setCustomValidity('')" required>
+                       oninput="this.setCustomValidity('')" required
+                       style="font-size: 14px">
             </div>
             <div class="form-group  mt-2">
                 <label for="inputEmail" class="sr-only"></label>
-                <input type="email" class="form-control" name="email" placeholder="email">
+                <input type="email" class="form-control" name="email" placeholder="email"
+                       style="font-size: 14px">
             </div>
             <div class="form-group  mt-2">
                 <label for="inputEmail" class="sr-only">Mức đầu tư dự kiến:</label>
-                <input type="text" class="form-control"  placeholder="Mức đầu tư dự kiến" name="invest">
+                <input type="text" class="form-control"  placeholder="Mức đầu tư dự kiến" name="invest"
+                       style="font-size: 14px">
             </div>
             <div class="form-group mt-2">
                 <label for="inputEmail" class="sr-only"></label>
-                <textarea class="form-control form-control-last" name="care" placeholder="Anh/chị đang quan tâm tới vấn đề gì của chương trình? (loại hình căn hộ, thủ tục đăng ký, hợp đồng, v.v.)"></textarea>
+                <textarea style="font-size: 14px" class="form-control form-control-last" name="care" placeholder="Anh/chị đang quan tâm tới vấn đề gì của chương trình? (loại hình căn hộ, thủ tục đăng ký, hợp đồng, v.v.)"></textarea>
             </div>
             <div class="form-group mt-2 ">
                 <button type="submit" class="btn btn-submit form-control d-flex justify-content-center align-items-center" style="background-color: #E7C57E">
@@ -215,30 +219,30 @@
             </div>
         </form>
     </div>
-    <div class="p-5  " style="width: 100%;">
+    <div class="ps-5 pe-5 pb-5  " style="width: 100%; margin-bottom: 15%">
         <div class="">
             <div>
                 <img src="{{asset('images/sec10/donvidoitac1.svg')}}" alt="" class="" style="width: 40%">
             </div>
             <div>
-                <img src="{{asset('images/sec10/cen-logo.svg')}}" alt="" class="cen-logo" style="width: 50%">
+                <img src="{{asset('images/sec10/cen-logo.svg')}}" alt="" class="cen-logo" style="width: 60%">
                 <img src="{{asset('images/sec10/danube.svg')}}" alt="" class="cen-logo"
-                     style="width: 50%; margin-top: -5%">
+                     style="width: 60%; margin-top: -5%">
             </div>
             <div>
-                <img src="{{asset('images/sec10/lienhevoichungtoi1.svg')}}" alt="" class="mt-5" style="width: 50%">
+                <img src="{{asset('images/sec10/lienhevoichungtoi1.svg')}}" alt="" class="mt-2" style="width: 50%">
             </div>
             <div class="mt-5">
                 <img src="{{asset('images/sec10/phone.svg')}}" alt="" class="" style="width: 5%">
-                <span class="text-white"> 096 569 1688</span>
+                <span class=" font-15 text-white mulish-semi"> 096 569 1688</span>
             </div>
             <div class="mt-2">
                 <img src="{{asset('images/sec10/mail.svg')}}" alt="" class="" style="width: 5%">
-                <span class="text-white font-15" > huyynguyentherealadvisor@gmail.com</span>
+                <span class="text-white font-15 mulish-semi" > huyynguyentherealadvisor@gmail.com</span>
             </div>
             <div class="mt-2">
                 <img src="{{asset('images/sec10/map.svg')}}" alt="" class="" style="width: 5%">
-                <span class="text-white font-15"> Tầng 1 - 137  Nguyễn Ngọc Vũ</span>
+                <span class="text-white font-15 mulish-semi"> Tầng 1 - 137  Nguyễn Ngọc Vũ</span>
                 <br>
                 <span class="text-white font-15"> Trung Hòa, Cầu Giấy, Hà Nội</span>
             </div>
@@ -268,7 +272,7 @@
                 processData: false,
                 success: function(response) {
                     // Hiển thị thông báo thành công
-                    alert('Thông tin đã được gửi thành công! Xin cảm ơn quý khách hàng đã quan tâm /-heart ');
+                    alert('Thông tin đã được gửi thành công! Xin cảm ơn quý khách hàng đã quan tâm');
                 },
                 error: function(xhr, status, error) {
                     // Xử lý lỗi nếu có
