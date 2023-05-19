4d2925d5ae283f50bf014babf3d0d1167b83de2f
Chien.le <chien.hcckt@gmail.com>
2023-05-19 10:47:01 +0700

    change controller to router throttle

--- app/Http/Controllers/SendController.php
+++ app/Http/Controllers/SendController.php
@@ -5,20 +5,18 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Http\Request;
 use Revolution\Google\Sheets\Facades\Sheets;
 use Google\Service\Exception;
+use Illuminate\Support\Carbon;
 
 class SendController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('throttle:5,30')->only('index');
-    }
     public function index(Request $request)
     {
+
+
         Sheets::spreadsheet(config('sheet.spreadsheet_id'));
         $rows = [$request->name, $request->phone, $request->email, $request->invest, $request->care];
 
         $rows = array($rows);
             Sheets::sheet('chien')->append($rows);
-            return redirect()->route('index');
     }
 }
